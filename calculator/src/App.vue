1d411b22b81f4220e58e639473465d94af98e84d
여운 <yeoun0570@gmail.com>
2024-10-15 16:20:35 +0900

    calculator실습 초기세팅

---
+++ calculator/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      output: true,
+      prev: true,
+      cur: true,
+      operator: true,
+    }
+  },
+
+  methods: {
+
+  },
+
+}
+</script>
+
+<template>
+  <p>test</p>
+</template>
+
+<style>
+  @import './assets/style.css';
+</style>
